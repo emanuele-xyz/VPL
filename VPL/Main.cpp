a2c2560a7bc98ffaf5d3302dd47fcac8b221798f
emanuele franchi <me.emanuele.xyz@gmail.com>
2025-06-11 11:25:34 +0200

    initialize d3d11

--- VPL/Main.cpp
+++ VPL/Main.cpp
@@ -1,11 +1,39 @@
+// ----------------------------------------------------------------------------
+// Includes
+// ----------------------------------------------------------------------------
+
+// Precompiled header
 #include "PCH.h"
 
+// Windows
 #define WIN32_LEAN_AND_MEAN
 #define NOMINMAX
 #include <Windows.h>
 #include <Windowsx.h>
-#include <wrl/client.h> // NOTE: for ComPtr
+#include <wrl/client.h> // for ComPtr
 namespace wrl = Microsoft::WRL;
+
+// D3D11
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <dxgi1_3.h>
+#if defined(_DEBUG)
+#include <dxgidebug.h>
+#endif
+
+// ----------------------------------------------------------------------------
+// Libraries
+// ----------------------------------------------------------------------------
+
+// D3D11
+#pragma comment(lib, "d3d11")
+#pragma comment(lib, "d3dcompiler")
+#pragma comment(lib, "dxgi")
+#pragma comment(lib, "dxguid")
+
+// ----------------------------------------------------------------------------
+// Program constants
+// ----------------------------------------------------------------------------
 
 constexpr const char* WINDOW_CLASS_NAME{ "vpl_window_class" };
 constexpr const char* WINDOW_TITLE{ "VPL" };
@@ -13,6 +41,10 @@ constexpr DWORD WINDOW_STYLE{ WS_OVERLAPPEDWINDOW };
 constexpr DWORD WINDOW_STYLE_EX{ WS_EX_OVERLAPPEDWINDOW };
 constexpr int WINDOW_START_W{ 1280 };
 constexpr int WINDOW_START_H{ 720 };
+
+// ----------------------------------------------------------------------------
+// Custom Assertions
+// ----------------------------------------------------------------------------
 
 class Error : public std::runtime_error
 {
@@ -27,6 +59,66 @@ public:
 #else
 #define Check(p) do { if (!(p)) throw Error(__FILE__, __LINE__, "check failed: " #p); } while (false)
 #endif
+
+#define CheckHR(hr) Check(SUCCEEDED(hr))
+
+// ----------------------------------------------------------------------------
+// Miscellaneous Utilities
+// ----------------------------------------------------------------------------
+
+static std::string StrFromWStr(const std::wstring& wstr)
+{
+    int buf_size{ WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), -1, nullptr, 0, nullptr, nullptr) };
+    if (buf_size > 0)
+    {
+        std::unique_ptr<char[]> buf{ std::make_unique<char[]>(buf_size) };
+        int written{ WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), -1, buf.get(), buf_size, nullptr, nullptr) };
+        Check(written == buf_size);
+        return buf.get();
+    }
+    else
+    {
+        return "";
+    }
+}
+
+static std::wstring WStrFromStr(const std::string& wstr)
+{
+    int buf_size{ MultiByteToWideChar(CP_UTF8, 0, wstr.c_str(), -1, nullptr, 0) };
+    if (buf_size > 0)
+    {
+        std::unique_ptr<wchar_t[]> buf{ std::make_unique<wchar_t[]>(buf_size) };
+        int written{ MultiByteToWideChar(CP_UTF8, 0, wstr.c_str(), -1, buf.get(), buf_size) };
+        Check(written == buf_size);
+        return buf.get();
+    }
+    else
+    {
+        return L"";
+    }
+}
+
+static std::string GetBytesStr(size_t bytes)
+{
+    const char* suffixes[]{ "B", "KB", "MB", "GB", "TB", "PB" };
+
+    size_t i{};
+    double val{ static_cast<double>(bytes) };
+    while (val >= 1024.0 && i < std::size(suffixes) - 1)
+    {
+        val /= 1024.0;
+        i++;
+    }
+
+    std::ostringstream out{};
+    out << std::fixed << std::setprecision(2) << val << " " << suffixes[i];
+
+    return out.str();
+}
+
+// ----------------------------------------------------------------------------
+// Window Procedure
+// ----------------------------------------------------------------------------
 
 static LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
@@ -44,6 +136,10 @@ static LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT message, WPARAM wParam, 
     }
     return result;
 }
+
+// ----------------------------------------------------------------------------
+// Win32 API Helpers
+// ----------------------------------------------------------------------------
 
 static void RegisterWin32WindowClass()
 {
@@ -87,14 +183,118 @@ static HWND CreateWin32Window()
     return hwnd;
 }
 
+// ----------------------------------------------------------------------------
+// D3D11 (and DXGI) API Helpers
+// ----------------------------------------------------------------------------
+
+static void SetupDXGIInforQueue()
+{
+    #if defined(_DEBUG)
+    wrl::ComPtr<IDXGIInfoQueue> queue{};
+    CheckHR(DXGIGetDebugInterface1(0, IID_PPV_ARGS(queue.ReleaseAndGetAddressOf())));
+    CheckHR(queue->SetBreakOnSeverity(DXGI_DEBUG_ALL, DXGI_INFO_QUEUE_MESSAGE_SEVERITY_CORRUPTION, true));
+    CheckHR(queue->SetBreakOnSeverity(DXGI_DEBUG_ALL, DXGI_INFO_QUEUE_MESSAGE_SEVERITY_ERROR, true));
+    #endif
+}
+
+static wrl::ComPtr<ID3D11Device> CreateD3D11Device()
+{
+
+    UINT flags{};
+    #if defined(_DEBUG)
+    flags |= D3D11_CREATE_DEVICE_DEBUG;
+    #endif
+    D3D_FEATURE_LEVEL requested_level{ D3D_FEATURE_LEVEL_11_1 };
+    D3D_FEATURE_LEVEL supported_level{};
+
+    wrl::ComPtr<ID3D11Device> d3d_dev{};
+    CheckHR(D3D11CreateDevice(
+        nullptr,
+        D3D_DRIVER_TYPE_HARDWARE,
+        nullptr,
+        flags,
+        &requested_level, 1,
+        D3D11_SDK_VERSION,
+        d3d_dev.ReleaseAndGetAddressOf(),
+        &supported_level,
+        nullptr
+    ));
+
+    Check(requested_level == supported_level);
+
+    return d3d_dev;
+}
+
+static void SetupD3D11InfoQueue([[maybe_unused]] ID3D11Device* d3d_dev)
+{
+    #if defined(_DEBUG)
+    wrl::ComPtr<ID3D11InfoQueue> queue{};
+    CheckHR(d3d_dev->QueryInterface(queue.ReleaseAndGetAddressOf()));
+    CheckHR(queue->SetBreakOnSeverity(D3D11_MESSAGE_SEVERITY_CORRUPTION, true));
+    CheckHR(queue->SetBreakOnSeverity(D3D11_MESSAGE_SEVERITY_ERROR, true));
+    #endif
+}
+
+static wrl::ComPtr<IDXGISwapChain1> CreateDXGISwapChain(HWND window, ID3D11Device* d3d_dev)
+{
+    wrl::ComPtr<IDXGIDevice> dxgi_dev{};
+    CheckHR(d3d_dev->QueryInterface(dxgi_dev.ReleaseAndGetAddressOf()));
+    wrl::ComPtr<IDXGIAdapter> dxgi_adapter{};
+    CheckHR(dxgi_dev->GetAdapter(dxgi_adapter.ReleaseAndGetAddressOf()));
+
+    DXGI_ADAPTER_DESC adapter_desc{};
+    CheckHR(dxgi_adapter->GetDesc(&adapter_desc));
+
+    std::cout << "adapter: " << StrFromWStr(adapter_desc.Description) << std::endl;
+    std::cout << "VRAM: " << GetBytesStr(adapter_desc.DedicatedVideoMemory) << std::endl;
+    std::cout << "dedicated RAM: " << GetBytesStr(adapter_desc.DedicatedSystemMemory) << std::endl;
+    std::cout << "shared RAM: " << GetBytesStr(adapter_desc.SharedSystemMemory) << std::endl;
+
+    wrl::ComPtr<IDXGIFactory2> dxgi_factory{};
+    CheckHR(dxgi_adapter->GetParent(IID_PPV_ARGS(dxgi_factory.ReleaseAndGetAddressOf())));
+
+    DXGI_SWAP_CHAIN_DESC1 desc{};
+    desc.Width = 0; // use window width
+    desc.Height = 0; // use window height
+    desc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+    desc.Stereo = false;
+    desc.SampleDesc = { .Count = 1, .Quality = 0 };
+    desc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    desc.BufferCount = 2; // double buffering
+    desc.Scaling = DXGI_SCALING_NONE;
+    desc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+    desc.AlphaMode = DXGI_ALPHA_MODE_UNSPECIFIED;
+    desc.Flags = 0;
+
+    wrl::ComPtr<IDXGISwapChain1> swap_chain{};
+    CheckHR(dxgi_factory->CreateSwapChainForHwnd(d3d_dev, window, &desc, nullptr, nullptr, swap_chain.ReleaseAndGetAddressOf()));
+
+    // disable Alt+Enter changing monitor resolution to match window size
+    CheckHR(dxgi_factory->MakeWindowAssociation(window, DXGI_MWA_NO_ALT_ENTER));
+
+    return swap_chain;
+}
+
+// ----------------------------------------------------------------------------
+// Application's Entry Point (may throw an exception)
+// ----------------------------------------------------------------------------
+
 static void Entry()
 {
-    // win32 initialization
+    // Win32 Initialization
     Check(SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_SYSTEM_AWARE));
     RegisterWin32WindowClass();
     [[maybe_unused]] HWND window{ CreateWin32Window() };
 
-    // main loop
+    // D3D11 Initialization
+    SetupDXGIInforQueue();
+    wrl::ComPtr<ID3D11Device> d3d_dev{ CreateD3D11Device() };
+    wrl::ComPtr<ID3D11DeviceContext> d3d_ctx{};
+    d3d_dev->GetImmediateContext(d3d_ctx.ReleaseAndGetAddressOf());
+    SetupD3D11InfoQueue(d3d_dev.Get());
+    wrl::ComPtr<IDXGISwapChain1> swap_chain{ CreateDXGISwapChain(window, d3d_dev.Get()) };
+
+    // Main Loop
     {
         MSG msg{};
         while (msg.message != WM_QUIT)
@@ -106,11 +306,15 @@ static void Entry()
             }
             else
             {
-                // TODO: update logic here
+                // TODO: Update Logic
             }
         }
     }
 }
+
+// ----------------------------------------------------------------------------
+// Main
+// ----------------------------------------------------------------------------
 
 int main()
 {
--- VPL/PCH.h
+++ VPL/PCH.h
@@ -1,7 +1,16 @@
 #pragma once
 
+#include <algorithm>
+#include <cstddef>
+#include <cstring>
+#include <exception>
+#include <filesystem>
 #include <format>
+#include <iomanip>
 #include <iostream>
+#include <memory>
+#include <sstream>
 #include <stacktrace>
 #include <stdexcept>
 #include <string>
+#include <vector>
