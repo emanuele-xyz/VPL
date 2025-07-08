c376d03c1afbcf5b4a6c68bd759518ed56c347fd
emanuele franchi <me.emanuele.xyz@gmail.com>
2025-07-08 12:19:40 +0200

    change shadow cube map sampler from linear to point

--- VPL/Main.cpp
+++ VPL/Main.cpp
@@ -1244,7 +1244,7 @@ static void Entry()
     wrl::ComPtr<ID3D11SamplerState> ss_cube_shadow_map{};
     {
         D3D11_SAMPLER_DESC desc{};
-        desc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR; // TODO: point
+        desc.Filter = D3D11_FILTER_MIN_MAG_MIP_POINT;
         desc.AddressU = D3D11_TEXTURE_ADDRESS_BORDER;
         desc.AddressV = D3D11_TEXTURE_ADDRESS_BORDER;
         desc.AddressW = D3D11_TEXTURE_ADDRESS_BORDER;
